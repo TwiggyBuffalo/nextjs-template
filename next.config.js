5fccf1f296154b0f17ed22624aeb21c814b9a551
TwiggyBuffalo <danielcurran@live.co.za>
2020-09-04 15:58:13 +0200

    init

---
+++ next.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+const {NODE_ENV} = process.env
+
+const isProd = NODE_ENV === 'production'
+
+const nextConfig = {
+	webpack: (config, {isServer}) => {
+		if (!isServer) {
+			config.node = {
+				fs: 'empty'
+			}
+		}
+
+		config.resolve.alias.components = path.join(__dirname, 'components')
+		config.resolve.alias.images = path.join(__dirname, 'public/static/images')
+
+		if (isProd) {
+			config.performance = {
+				hints: 'error',
+				maxEntrypointSize: 20000000,
+				maxAssetSize: 20000000
+			}
+		}
+
+		return config
+	}
+}
+
+module.exports = nextConfig
---
+++ xo.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+	extends: ['plugin:jsx-a11y/strict', 'xo-react'],
+	plugins: 'jsx-a11y',
+	envs: ['es2020', 'browser'],
+	prettier: true,
+	semicolon: false,
+	nodeVersion: '12',
+	rules: {
+		'unicorn/no-process-exit': 'warn',
+		'import/no-unassigned-import': 'off', // We import css/scss files globally in some cases
+		'import/extensions': 'off',
+
+		'jsx-a11y/anchor-is-valid': 'off', // Next.js has its own implementation of link
+		'jsx-a11y/no-onchange': 'warn',
+		'jsx-a11y/label-has-for': 'off', // Not picking up the htmlFor property
+
+		'@typescript-eslint/restrict-template-expressions': 'off',
+		'@typescript-eslint/ban-types': 'off'
+	},
+	overrides: [
+		{
+			files: './**/*.tsx',
+			rules: {
+				'react/prop-types': 'off' // Not needed for TS
+			}
+		},
+		{
+			files: 'components/**/*',
+			rules: {
+				'node/no-unsupported-features/es-syntax': 'off' // Dynamic imports
+			}
+		},
+		{
+			files: 'pages/**/*',
+			rules: {
+				'node/no-unsupported-features/es-syntax': 'off' // Dynamic imports
+			}
+		},
+		{
+			files: './**/*.test.tsx',
+			envs: ['es2020', 'node', 'browser', 'jest']
+		}
+	]
+}
